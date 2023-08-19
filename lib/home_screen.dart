54c6059dff53f97030c3847a8d71ae4071a7f018
Jeremy Taraba <jeremytaraba@gmail.com>
2023-08-18 18:02:58 -0700

    Working on homescreen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -5,6 +5,19 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.library_books), label: "History"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.camera_alt_outlined), label: "Camera"),
+        ],
+      ),
+    );
   }
 }
